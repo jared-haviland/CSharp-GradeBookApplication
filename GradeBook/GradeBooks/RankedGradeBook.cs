477fcb107a80789a4c2109c35fb14f431c0dad1b
Jared Haviland <jared.haviland@cerner.com>
2021-10-09 10:52:11 -0500

    Implement GetLetterGrade in RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,38 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int studentsWithHigherGrade = 0;
+            foreach (Student s in Students)
+            {
+                if (s.AverageGrade > averageGrade)
+                {
+                    studentsWithHigherGrade++;
+                }
+            }
+
+            int twentyPercentOfStudents = Students.Count / 5;
+            int gradeLevelsToDrop = studentsWithHigherGrade / twentyPercentOfStudents;
+            switch (gradeLevelsToDrop)
+            {
+                case 0:
+                    return 'A';
+                case 1:
+                    return 'B';
+                case 2:
+                    return 'C';
+                case 3:
+                    return 'D';
+                default:
+                    return 'F';
+            }
+        }
     }
 }
